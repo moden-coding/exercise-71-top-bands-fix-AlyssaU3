0e0c166bfd6eb6008a75433d9895ef04073ce54d
AlyssaU3 <alys4321c@gmail.com>
2025-11-07 10:06:28 -0500

    71 done

--- src/top_bands.py
+++ src/top_bands.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def top_bands():
-    pass
-
+    df= pd.read_csv("src/bands.tsv", sep=("\t"))
+    records= pd.read_csv("src/UK-top40-1964-1-2.tsv", sep=("\t"))
+    df["Band"]=df["Band"].str.upper() 
+    merged= pd.merge(df,records, left_on=("Band"), right_on=("Artist"), how="outer")
+    merged=merged.dropna()
+    return merged
 def main():
-    top_bands()
+    print(top_bands())
 
 if __name__ == "__main__":
     main()
